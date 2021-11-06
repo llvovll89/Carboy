1c1cb97eb4aff56c4c8ef2e9953b416a233e902d
llvovll89 <80821441+llvovll89@users.noreply.github.com>
2021-11-06 19:33:15 +0900

    Update scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,6 @@
 let curPos = 0; // 현재 보여주는 이미지의 인덱스 번호
 let positionValue = 0; // 이미지 태그의 위치 값 지정할 변수
-const IMAGE_WIDTH = 450; // 이미지 사이즈 만큼 이동한다
+const IMAGE_WIDTH = 350; // 이미지 사이즈 만큼 이동한다
 
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
